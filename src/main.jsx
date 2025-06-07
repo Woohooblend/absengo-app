fb07b4b4aa6c20328d19c1984a6441ecc3c9b797
Gabrielle Angela <ayopeps@gmail.com>
2025-06-07 15:21:11 +0700

    Test dashboard page

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App.jsx";
- // pastikan ini pathnya sesuai file asli kamu
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
